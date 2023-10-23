7986e2ec77a93a2ea448594134a3951a8b06944b
barrettxavier <barrettxavier@yahoo.com>
2023-10-23 15:35:35 -0400

    added index.js index.html and a style.css

---
+++ index.js
@@ -0,0 +1,20 @@
+/* Create a string for a pop up message that says "You have received this message because you have
+    been chosen to open an important vault." */
+// Assign three variables. 
+// Each variable contains the corresponding result of calculation using a unique arithmetic operator(+-*/).
+// Each must equal one of the three codes in the combination.
+// Create a dialog box displaying the vault codes.
+// Sumbit file with code and pseudocode
+
+const message = `You have received this message because you have been chosen to open an important vault!`;
+alert(message)
+
+const code1 = 5 * 2;
+const code2 = 80 / 2;
+const code3 = 40 - 1;
+
+console.log(code1);
+console.log(code2);
+console.log(code3);
+
+alert(`The vault combination is ${code1}, ${code2}, ${code3}`);
